519d87c6a205286ca8858ee5da5d7c8c23a3a084
mokupsas <mockus.ri@gmail.com>
2022-11-14 18:49:42 +0200

    updated ProductController with show()

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,25 +7,42 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ProductController extends AbstractController
 {
     #[Route('/product', name: 'create_product')]
-    public function createProduct(ManagerRegistry $doctrine): Response
+    public function createProduct(ValidatorInterface $validator): Response
     {
-        $entityManager = $doctrine->getManager();
-
         $product = new Product();
-        $product->setName('Keyboard');
-        $product->setPrice(1999);
-        $product->setDescription('Ergonomic and stylish!');
+        // This will trigger an error: the column isn't nullable in the database
+        $product->setName('Name');
+        // This will trigger a type mismatch error: an integer is expected
+        $product->setPrice('1999');
 
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($product);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
+        $errors = $validator->validate($product);
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
 
         return new Response('Saved new product with id '.$product->getId());
+    }
+
+    #[Route('/product/{id}', name: 'product_show')]
+    public function show(ManagerRegistry $doctrine, int $id): Response
+    {
+        $product = $doctrine->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great product: '.$product->getName());
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
     }
 }
